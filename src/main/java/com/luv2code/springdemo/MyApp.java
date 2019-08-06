d76b11b4c4aa046737e04ece0b3b80429aa5e1e0
Andrey, Orlov <ANOrlov@luxoft.com>
2019-08-07 01:28:49 +0300

    commit with first lessons on my courses

---
+++ src/main/java/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,18 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // create the object
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        // use the object
+        System.out.println(theCoach.getDailyWorkout());
+        context.close();
+    }
+
+}
